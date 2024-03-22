8ebeeaf859d1baf4595818c33a1cf53302ff2726
Hugo Ramon <gustavo@gltecnologia.com.br>
2024-03-22 17:53:52 -0300

    Added query params in the routes

--- src/components/teams/TeamsItem.vue
+++ src/components/teams/TeamsItem.vue
@@ -12,7 +12,7 @@ export default {
   props: ['id', 'name', 'memberCount'],
   computed: {
     teamMembersLink() {
-      return { name: 'team-members', params: { teamId: this.id } }
+      return { name: 'team-members', params: { teamId: this.id }, query: { sort: 'asc' } }
     }
   }
 };
